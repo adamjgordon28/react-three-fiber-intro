6f0554731ea102a9958fec8edbd18b8e6c7a8443
Adam Gordon <adamjgordon24@gmail.com>
2022-09-26 00:21:35 -0400

    finish video 11

--- src/App.js
+++ src/App.js
@@ -14,13 +14,31 @@ const Orbit = () => {
 const Box = (props) => {
   const ref = useRef();
   useFrame((state) => {
-    ref.current.rotation.x += 0.01;
+    // ref.current.rotation.x += 0.01;
     ref.current.rotation.y += 0.01;
   });
   return (
-    <mesh ref={ref} {...props} castShadow receiveShadow>
+    <mesh
+      ref={ref}
+      {...props}
+      castShadow
+      // receiveShadow
+    >
       <boxGeometry />
-      <meshPhysicalMaterial color="blue" />
+      <meshPhysicalMaterial
+        color="white"
+        // fog // determines whether item is hidden by fog
+        opacity={1} // defaults to 0
+        transparent // defaults to false but makes opacity active
+        // visible // defaults to true
+        // wireframe={false} // appears as a wireframe of shape's geometry
+        // metalness={0} // metal-like surfaces should be 1
+        roughness={0} // smooth and shiny vs rough and un-reflective
+        clearcoat={1} // makes even shinier
+        transmission={1} // Degree of transmission (or optical transparency), from 0.0 to 1.0. Default is 0.0. Thin, transparent or semitransparent, plastic or glass materials remain largely reflective even if they are fully transmissive. The transmission property can be used to model these materials. When transmission is non-zero, opacity should be set to 0.
+        reflectivity={0.5} // Degree of reflectivity, from 0.0 to 1.0. Default is 0.5, which corresponds to an index-of-refraction of 1.5. This models the reflectivity of non-metallic materials. It has no effect when metalness is 1.0
+        side={THREE.DoubleSide}
+      />
     </mesh>
   );
 };
@@ -50,14 +68,15 @@ function App() {
       <Canvas
         shadows
         style={{ background: "black" }}
-        camera={{ position: [3, 3, 3] }}
+        camera={{ position: [1, 5, 1] }}
       >
+        {/* <fog attach="fog" color="white" near={1} far={10} /> */}
         <ambientLight intensity={0.2} />
 
         <Bulb position={[0, 3, 0]} />
         <Orbit />
         <axesHelper args={[5]} />
-        <Box position={[-1, 1, 2]} />
+        <Box position={[0, 1, 0]} />
         <Floor position={[0, -0.5, 0]} />
       </Canvas>
     </div>
