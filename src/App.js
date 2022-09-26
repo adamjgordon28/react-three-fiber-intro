62c9004496f777baa94da36496e1787a2a74929d
Adam Gordon <adamjgordon24@gmail.com>
2022-09-26 00:51:27 -0400

    finish video 12

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
-import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
-import { useRef, useMemo } from "react";
+import {
+  Canvas,
+  useFrame,
+  useThree,
+  extend,
+  useLoader,
+} from "@react-three/fiber";
+import { useRef, Suspense } from "react";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as THREE from "three";
 extend({ OrbitControls });
@@ -13,8 +19,9 @@ const Orbit = () => {
 
 const Box = (props) => {
   const ref = useRef();
+  const texture = useLoader(THREE.TextureLoader, "/wood.jpeg");
   useFrame((state) => {
-    // ref.current.rotation.x += 0.01;
+    ref.current.rotation.x += 0.01;
     ref.current.rotation.y += 0.01;
   });
   return (
@@ -24,21 +31,9 @@ const Box = (props) => {
       castShadow
       // receiveShadow
     >
-      <boxGeometry />
-      <meshPhysicalMaterial
-        color="white"
-        // fog // determines whether item is hidden by fog
-        opacity={1} // defaults to 0
-        transparent // defaults to false but makes opacity active
-        // visible // defaults to true
-        // wireframe={false} // appears as a wireframe of shape's geometry
-        // metalness={0} // metal-like surfaces should be 1
-        roughness={0} // smooth and shiny vs rough and un-reflective
-        clearcoat={1} // makes even shinier
-        transmission={1} // Degree of transmission (or optical transparency), from 0.0 to 1.0. Default is 0.0. Thin, transparent or semitransparent, plastic or glass materials remain largely reflective even if they are fully transmissive. The transmission property can be used to model these materials. When transmission is non-zero, opacity should be set to 0.
-        reflectivity={0.5} // Degree of reflectivity, from 0.0 to 1.0. Default is 0.5, which corresponds to an index-of-refraction of 1.5. This models the reflectivity of non-metallic materials. It has no effect when metalness is 1.0
-        side={THREE.DoubleSide}
-      />
+      {/* <boxGeometry /> */}
+      <sphereGeometry args={[1, 100, 100]} />
+      <meshPhysicalMaterial map={texture} />
     </mesh>
   );
 };
@@ -62,13 +57,30 @@ const Bulb = (props) => {
   );
 };
 
+const Background = (props) => {
+  // const { gl } = useThree();
+  const texture = useLoader(THREE.TextureLoader, "/aquaduct.jpeg");
+
+  texture.encoding = THREE.sRGBEncoding;
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+
+  // wasn't working
+  // const formatted = new THREE.WebGLCubeRenderTarget(
+  //   texture.image.height
+  // ).fromEquirectangularTexture(gl, texture);
+
+  // console.log({ formatted });
+
+  return <primitive attach="background" object={texture} />;
+};
+
 function App() {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <Canvas
         shadows
         style={{ background: "black" }}
-        camera={{ position: [1, 5, 1] }}
+        camera={{ position: [3, 3, 3] }}
       >
         {/* <fog attach="fog" color="white" near={1} far={10} /> */}
         <ambientLight intensity={0.2} />
@@ -76,8 +88,14 @@ function App() {
         <Bulb position={[0, 3, 0]} />
         <Orbit />
         <axesHelper args={[5]} />
-        <Box position={[0, 1, 0]} />
+        <Suspense fallback={null}>
+          <Box position={[0, 1, 0]} />
+        </Suspense>
+
         <Floor position={[0, -0.5, 0]} />
+        <Suspense fallback={null}>
+          <Background />
+        </Suspense>
       </Canvas>
     </div>
   );
